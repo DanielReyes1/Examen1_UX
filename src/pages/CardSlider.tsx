c164884ecc147ed08edd09cec8e93f50b2b2a7be
Daniel <danielreyesaleman4@gmail.com>
2024-11-11 13:49:32 -0600

    Carrusel arreglado solo me falto agregar mas carruseles

--- src/pages/CardSlider.tsx
+++ src/pages/CardSlider.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import Slider from 'react-slick'; 
-
-const Carousel = () => {
+interface CarouselProps{
+  titulo:string;
+  img1:string;
+  img2:string;
+  img3:string;
+  img4:string;
+  img5:string;
+  img6:string;
+  img7:string;
+  img8:string;
+  img9:string;
+  img10:string;
+  img11:string;
+  img12:string;
+}
+const Carousel = ({titulo,img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12}:CarouselProps) => {
   const settings = {
     dots: true,  // Muestra los puntos de navegación
     infinite: true,  // Hace que sea infinito
     speed: 500,  // Tiempo de transición entre elementos
-    slidesToShow: 5,  // Número de elementos a mostrar al mismo tiempo
-    slidesToScroll: 1,  // Cuántos elementos se desplazan al hacer scroll
+    slidesToShow: 6,  // Número de elementos a mostrar al mismo tiempo
+    slidesToScroll: 6,  // Cuántos elementos se desplazan al hacer scroll
     responsive: [
       {
         breakpoint: 1024,  // Para pantallas grandes
@@ -19,12 +33,14 @@ const Carousel = () => {
         breakpoint: 600,  // Para pantallas medianas
         settings: {
           slidesToShow: 2,
+          slidesToScroll: 2, 
         }
       },
       {
         breakpoint: 480,  // Para pantallas pequeñas
         settings: {
           slidesToShow: 1,
+          slidesToScroll: 1, 
         }
       }
     ]
@@ -32,29 +48,48 @@ const Carousel = () => {
 
   return (
     <div className="carousel-container" style={{backgroundColor:"rgb(10,10,10)"}}>
-       <p>Recomendado para ti</p> 
-       
-      <Slider {...settings}>
-        {}
-        <div>
-          <img src="https://hips.hearstapps.com/hmg-prod/images/5wducarmueomnh1wkca65pputxv-66ab60c945d48.jpg?crop=1xw:1xh;center,top&resize=980:*/150" />
+       <p style={{paddingTop:"10px",paddingLeft:"28px"}}>{titulo}</p> 
+       <div className="slider-wrapper" style={{ padding: "0 30px" }}>
+        <Slider {...settings}>
+          {}
+          <div>
+            <img src={img1} width={340} height={190} style={{paddingRight:"10px"}}/>
+          </div>
+          <div>
+            <img src={img2} width={340} height={190} style={{paddingRight:"10px"}}/>
+          </div>
+          <div>
+            <img src={img3} width={340} height={190}style={{paddingRight:"10px"}} />
+          </div>
+          <div>
+            <img src={img4}width={340} height={190}style={{paddingRight:"10px"}}/>
+          </div>
+          <div>
+            <img src={img5}width={340} height={190} style={{paddingRight:"10px"}}/>
+          </div>
+          <div>
+            <img src={img6} width={340} height={190} style={{paddingRight:"10px"}}/>
+          </div>
+          <div>
+            <img src={img7}width={340} height={190}style={{paddingRight:"10px"}} />
+          </div>
+          <div>
+            <img src={img8} width={340} height={190}style={{paddingRight:"10px"}} />
+          </div>
+          <div>
+            <img src={img9} width={340} height={190}style={{paddingRight:"10px"}} />
+          </div>
+          <div>
+            <img src={img10} width={340} height={190}style={{paddingRight:"10px"}} />
+          </div>
+          <div>
+            <img src={img11} width={340} height={190}style={{paddingRight:"10px"}} />
+          </div>
+          <div>
+            <img src={img12} width={340} height={190}style={{paddingRight:"10px"}} />
+          </div>
+        </Slider>
         </div>
-        <div>
-          <img src="https://media.revistagq.com/photos/66d03c2dbf5ddbc2c29dfc2b/master/w_1600%2Cc_limit/aku_wa_sonzai_shinai_ver2_xxlg.jpg/150" />
-        </div>
-        <div>
-          <img src="https://hips.hearstapps.com/hmg-prod/images/baby-reindeer-582365873-large-66aeda37dc5a8.jpg?crop=1xw:1xh;center,top&resize=980:*/150"  />
-        </div>
-        <div>
-          <img src="https://m.media-amazon.com/images/M/MV5BMTlmZjg1ZDctNjkwMi00NzU2LWFlYWMtZjdkMzMxYjViYmNhXkEyXkFqcGc@._V1_.jpg/150"  />
-        </div>
-        <div>
-          <img src="https://m.media-amazon.com/images/M/MV5BNTE1NmU1OTAtOGZhNC00ZmU4LWJiYWMtNGIzMjEzNzg0MGFmXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg/150"  />
-        </div>
-        <div>
-          <img src="https://media.revistagq.com/photos/633f024e2de46730aa112424/master/w_1600%2Cc_limit/midnight_club_ver2.jpg/150"  />
-        </div>
-      </Slider>
     </div>
   );
 };
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -8,15 +8,49 @@ import NavbarNetflix from "./navbar";
 import CardSlider from "./CardSlider";
 import Slider from "react-slick";
 import Billboard from "./billboard";
-
-
+import { useState } from "react";
+/*interface Deslizante{
+  titulo:string;
+  img1:string;
+  img2:string;
+  img3:string;
+  img4:string;
+  img5:string;
+  img6:string;
+  img7:string;
+  img8:string;
+  img9:string;
+  img10:string;
+  img11:string;
+  img12:string;
+}
+*/
 export default function Home() {
+  /*const [deslizante,setdeslizante]=useState<Deslizante[]>([
+    {
+      titulo:"Series de EE.UU. dobladas al español",
+      img1:"/img/us1.jpg",
+      img2:"/img/us2.jpg",
+      img3:"/img/us3.jpg",
+      img4:"/img/us4.jpg",
+      img5:"/img/us5.jpg",
+      img6:"/img/us6.jpg",
+      img7:"/img/us7.jpg",
+      img8:"/img/us8.jpg",
+      img9:"/img/us9.jpg",
+      img10:"/img/us10.jpg",
+      img11:"/img/us11.jpg",
+      img12:"/img/us12.jpg",
+
+    },
+  ]);*/
   return (
     <>
       <link rel="icon" href="favicon.ico"/>
       <NavbarNetflix/>
       <Billboard/>
-      <CardSlider/>
+      <CardSlider titulo="Series de EE.UU. dobladas al español" img1="/img/us1.jpg" img2="/img/us2.jpg" img3="/img/us3.jpg"img4="/img/us4.jpg"img5="/img/us5.jpg"img6="/img/us6.jpg"img7="/img/us7.jpg"img8="/img/us8.jpg"img9="/img/us9.jpg"img10="/img/us10.jpg"img11="/img/us11.jpg"img12="/img/us12.jpg"/>
+      <CardSlider titulo="Anime" img1="/img/anime1.jpg" img2="/img/anime2.jpg" img3="/img/anime3.jpg"img4="/img/anime4.jpg"img5="/img/anime5.jpg"img6="/img/anime6.jpg"img7="/img/anime7.jpg"img8="/img/anime8.jpg"img9="/img/anime9.jpg"img10="/img/anime10.jpg"img11="/img/anime11.jpg"img12="/img/anime12.jpg"/>
       <footer>© 1997-2024 Netflix, Inc.</footer>
     </>
   );
