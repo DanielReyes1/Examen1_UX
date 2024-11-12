02de7eaf2a281650ead21322af017be46ad26047
Daniel <danielreyesaleman4@gmail.com>
2024-11-11 19:37:07 -0600

    Ya esta todo el contenido con progress bars

--- src/pages/CardSlider.tsx
+++ src/pages/CardSlider.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Slider from 'react-slick'; 
+import ProgressBar from 'react-bootstrap/ProgressBar';
 interface CarouselProps{
   titulo:string;
   img1:string;
@@ -14,8 +15,11 @@ interface CarouselProps{
   img10:string;
   img11:string;
   img12:string;
+  barra: string;
+
 }
-const Carousel = ({titulo,img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12}:CarouselProps) => {
+const Carousel = ({titulo,img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img11,img12,barra}:CarouselProps) => {
+  
   const settings = {
     dots: true,  // Muestra los puntos de navegación
     infinite: true,  // Hace que sea infinito
@@ -27,6 +31,7 @@ const Carousel = ({titulo,img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img
         breakpoint: 1024,  // Para pantallas grandes
         settings: {
           slidesToShow: 3,
+          slidesToScroll: 3, 
         }
       },
       {
@@ -48,45 +53,58 @@ const Carousel = ({titulo,img1,img2,img3,img4,img5,img6,img7,img8,img9,img10,img
 
   return (
     <div className="carousel-container" style={{backgroundColor:"rgb(10,10,10)"}}>
-       <p style={{paddingTop:"10px",paddingLeft:"28px"}}>{titulo}</p> 
+       <p style={{paddingTop:"40px",paddingLeft:"28px"}}>{titulo}</p> 
        <div className="slider-wrapper" style={{ padding: "0 30px" }}>
         <Slider {...settings}>
           {}
           <div>
-            <img src={img1} width={340} height={190} style={{paddingRight:"10px"}}/>
+            <img src={img1} width={340} height={190} style={{paddingRight:"10px", borderRadius:"15px"}}/>
+            {barra && <ProgressBar variant="danger" now={80} className="barrita"  style={{height:"5px",width:"150px",backgroundColor:"rgb(88,88,88)"}}/>}
+                
           </div>
           <div>
-            <img src={img2} width={340} height={190} style={{paddingRight:"10px"}}/>
+            <img src={img2} width={340} height={190} style={{paddingRight:"10px", borderRadius:"15px"}}/>
+            {barra && <ProgressBar variant="danger" now={50} className="barrita"  style={{height:"5px",width:"150px",backgroundColor:"rgb(88,88,88)"}}/>}
           </div>
           <div>
-            <img src={img3} width={340} height={190}style={{paddingRight:"10px"}} />
+            <img src={img3} width={340} height={190}style={{paddingRight:"10px", borderRadius:"15px"}} />
+            {barra && <ProgressBar variant="danger" now={30} className="barrita"  style={{height:"5px",width:"150px",backgroundColor:"rgb(88,88,88)"}}/>}
           </div>
           <div>
-            <img src={img4}width={340} height={190}style={{paddingRight:"10px"}}/>
+            <img src={img4}width={340} height={190}style={{paddingRight:"10px", borderRadius:"15px"}}/>
+            {barra && <ProgressBar variant="danger" now={90} className="barrita"  style={{height:"5px",width:"150px",backgroundColor:"rgb(88,88,88)"}}/>}
           </div>
           <div>
-            <img src={img5}width={340} height={190} style={{paddingRight:"10px"}}/>
+            <img src={img5}width={340} height={190} style={{paddingRight:"10px", borderRadius:"15px"}}/>
+            {barra && <ProgressBar variant="danger" now={10} className="barrita"  style={{height:"5px",width:"150px",backgroundColor:"rgb(88,88,88)"}}/>}
           </div>
           <div>
-            <img src={img6} width={340} height={190} style={{paddingRight:"10px"}}/>
+            <img src={img6} width={340} height={190} style={{paddingRight:"10px", borderRadius:"15px"}}/>
+            {barra && <ProgressBar variant="danger" now={30} className="barrita"  style={{height:"5px",width:"150px",backgroundColor:"rgb(88,88,88)"}}/>}
           </div>
           <div>
-            <img src={img7}width={340} height={190}style={{paddingRight:"10px"}} />
+            <img src={img7}width={340} height={190}style={{paddingRight:"10px", borderRadius:"15px"}} />
+            {barra && <ProgressBar variant="danger" now={70} className="barrita"  style={{height:"5px",width:"150px",backgroundColor:"rgb(88,88,88)"}}/>}
           </div>
           <div>
-            <img src={img8} width={340} height={190}style={{paddingRight:"10px"}} />
+            <img src={img8} width={340} height={190}style={{paddingRight:"10px", borderRadius:"15px"}} />
+            {barra && <ProgressBar variant="danger" now={50} className="barrita"  style={{height:"5px",width:"150px",backgroundColor:"rgb(88,88,88)"}}/>}
           </div>
           <div>
-            <img src={img9} width={340} height={190}style={{paddingRight:"10px"}} />
+            <img src={img9} width={340} height={190}style={{paddingRight:"10px", borderRadius:"15px"}} />
+            {barra && <ProgressBar variant="danger" now={20} className="barrita"  style={{height:"5px",width:"150px",backgroundColor:"rgb(88,88,88)"}}/>}
           </div>
           <div>
-            <img src={img10} width={340} height={190}style={{paddingRight:"10px"}} />
+            <img src={img10} width={340} height={190}style={{paddingRight:"10px", borderRadius:"15px"}} />
+            {barra && <ProgressBar variant="danger" now={90} className="barrita"  style={{height:"5px",width:"150px",backgroundColor:"rgb(88,88,88)"}}/>}
           </div>
           <div>
-            <img src={img11} width={340} height={190}style={{paddingRight:"10px"}} />
+            <img src={img11} width={340} height={190}style={{paddingRight:"10px", borderRadius:"15px"}} />
+            {barra && <ProgressBar variant="danger" now={40} className="barrita"  style={{height:"5px",width:"150px",backgroundColor:"rgb(88,88,88)"}}/>}
           </div>
           <div>
-            <img src={img12} width={340} height={190}style={{paddingRight:"10px"}} />
+            <img src={img12} width={340} height={190}style={{paddingRight:"10px", borderRadius:"15px"}} />
+            {barra && <ProgressBar variant="danger" now={30} className="barrita"  style={{height:"5px",width:"150px",backgroundColor:"rgb(88,88,88)"}}/>}
           </div>
         </Slider>
         </div>
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Button, Container,Navbar, Nav, Collapse, Dropdown } from 
 'react-bootstrap';
+import ProgressBar from 'react-bootstrap/ProgressBar';
 import NavbarNetflix from "./navbar";
 //import Carrusel from "./Carrusel";
 import CardSlider from "./CardSlider";
@@ -49,8 +50,12 @@ export default function Home() {
       <link rel="icon" href="favicon.ico"/>
       <NavbarNetflix/>
       <Billboard/>
-      <CardSlider titulo="Series de EE.UU. dobladas al español" img1="/img/us1.jpg" img2="/img/us2.jpg" img3="/img/us3.jpg"img4="/img/us4.jpg"img5="/img/us5.jpg"img6="/img/us6.jpg"img7="/img/us7.jpg"img8="/img/us8.jpg"img9="/img/us9.jpg"img10="/img/us10.jpg"img11="/img/us11.jpg"img12="/img/us12.jpg"/>
-      <CardSlider titulo="Anime" img1="/img/anime1.jpg" img2="/img/anime2.jpg" img3="/img/anime3.jpg"img4="/img/anime4.jpg"img5="/img/anime5.jpg"img6="/img/anime6.jpg"img7="/img/anime7.jpg"img8="/img/anime8.jpg"img9="/img/anime9.jpg"img10="/img/anime10.jpg"img11="/img/anime11.jpg"img12="/img/anime12.jpg"/>
+      <CardSlider titulo="Continuar Viendo" img1="/img/continuar1.jpg" img2="/img/continuar2.jpg" img3="/img/continuar3.jpg"img4="/img/continuar4.jpg"img5="/img/continuar5.jpg"img6="/img/continuar6.jpg"img7="/img/continuar7.jpg"img8="/img/continuar8.jpg"img9="/img/continuar9.jpg"img10="/img/continuar10.jpg"img11="/img/continuar11.jpg"img12="/img/continuar12.jpg" barra="confirmado"/>
+      <CardSlider titulo="Series de EE.UU. dobladas al español" img1="/img/us1.jpg" img2="/img/us2.jpg" img3="/img/us3.jpg"img4="/img/us4.jpg"img5="/img/us5.jpg"img6="/img/us6.jpg"img7="/img/us7.jpg"img8="/img/us8.jpg"img9="/img/us9.jpg"img10="/img/us10.jpg"img11="/img/us11.jpg"img12="/img/us12.jpg" barra=""/>
+      <CardSlider titulo="Anime" img1="/img/anime1.jpg" img2="/img/anime2.jpg" img3="/img/anime3.jpg"img4="/img/anime4.jpg"img5="/img/anime5.jpg"img6="/img/anime6.jpg"img7="/img/anime7.jpg"img8="/img/anime8.jpg"img9="/img/anime9.jpg"img10="/img/anime10.jpg"img11="/img/anime11.jpg"img12="/img/anime12.jpg" barra=""/>
+      <CardSlider titulo="Favoritos familiares" img1="/img/familiar1.jpg" img2="/img/familiar2.jpg" img3="/img/familiar3.jpg"img4="/img/familiar4.jpg"img5="/img/familiar5.jpg"img6="/img/familiar6.jpg"img7="/img/familiar7.jpg"img8="/img/familiar8.jpg"img9="/img/familiar9.jpg"img10="/img/familiar10.jpg"img11="/img/familiar11.jpg"img12="/img/familiar12.jpg" barra=""/>
+      <CardSlider titulo="Solo en Netflix" img1="/img/solo1.jpg" img2="/img/solo2.jpg" img3="/img/solo3.jpg"img4="/img/solo4.jpg"img5="/img/solo5.jpg"img6="/img/solo6.jpg"img7="/img/solo7.jpg"img8="/img/solo8.jpg"img9="/img/solo9.jpg"img10="/img/solo10.jpg"img11="/img/solo11.jpg"img12="/img/solo12.jpg" barra=""/>
+      <CardSlider titulo="Películas de Comedia" img1="/img/comedia1.jpg" img2="/img/comedia2.jpg" img3="/img/comedia3.jpg"img4="/img/comedia4.jpg"img5="/img/comedia5.jpg"img6="/img/comedia6.jpg"img7="/img/comedia7.jpg"img8="/img/comedia8.jpg"img9="/img/comedia9.jpg"img10="/img/comedia10.jpg"img11="/img/comedia11.jpg"img12="/img/comedia12.jpg" barra=""/>
       <footer>© 1997-2024 Netflix, Inc.</footer>
     </>
   );
